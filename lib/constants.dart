4967c85813483476ea0801951103806dc877efef
soulo <tsoulo@student.wethinkcode.co.za>
2023-05-22 00:19:55 +0200

    env edit

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-const String google_api_key = "AIzaSyCLj0hGynYN1ZkKqb5P1ywHJ01ffvK77XY";
+const String google_api_key = "ApiKey";
 const Color primaryColor = Color(0xFF7B61FF);
 const double defaultPadding = 16.0;
 const int updateSeconds = 10;
